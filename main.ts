e523a40e3cf1461790cb76bea6b8106194931c5a
aloprui26-06 <102860876+aloprui26-06@users.noreply.github.com>
2022-04-29 20:29:26 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
 let sonido = 0
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    `)
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+basic.pause(2000)
+basic.clearScreen()
 basic.forever(function () {
     sonido = input.soundLevel()
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
